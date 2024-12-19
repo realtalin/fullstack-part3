708c8c9ec398847afab5c87dd6c2857995712874
Sampo <57003465+realtalin@users.noreply.github.com>
2024-12-19 23:51:44 +0200

    exercise 3.8

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {

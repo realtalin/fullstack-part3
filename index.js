e869bcc9474e4d2df1a58d845b1fa57909f621e5
Sampo <57003465+realtalin@users.noreply.github.com>
2024-12-22 17:20:16 +0200

    exercise 3.19

--- index.js
+++ index.js
@@ -26,12 +26,6 @@ app.get('/api/persons', (_, response) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number missing',
-    })
-  }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -53,7 +47,11 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson)
     })
@@ -96,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformed request' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)

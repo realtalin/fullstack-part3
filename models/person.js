e3632980eb68fba2bb7b6d4f963ffa602d945099
Sampo <57003465+realtalin@users.noreply.github.com>
2024-12-22 17:31:37 +0200

    exercise 3.20

--- models/person.js
+++ models/person.js
@@ -24,6 +24,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    minLength: 8,
+    validate: {
+      validator: (number) => {
+        return /\d{2,3}-\d{6,}/.test(number)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
   },
 })
 
